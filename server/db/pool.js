064b957ce85fa5682a40157cdf58ae836be2c73d
iamtati1 <tatianabarmer@gmail.com>
2026-06-14 13:54:51 -0400

    fix postgres pool production config

--- server/db/pool.js
+++ server/db/pool.js
@@ -1,20 +1,15 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const isProduction = process.env.DATABASE_URL;
+const isProduction = process.env.NODE_ENV === "production";
 
 const pool = isProduction
   ? new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    ssl: { rejectUnauthorized: false },
   })
   : new Pool({
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
-    user: process.env.PGUSER,
+    connectionString: process.env.DATABASE_URL || undefined,
   });
 
 module.exports = pool;
